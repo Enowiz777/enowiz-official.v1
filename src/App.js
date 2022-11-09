e66e7aba31d93e1a32ba698c4a38a57dc469ec4f
Enoch Park <enoch.park@enowiz.com>
2022-11-08 19:50:59 -0800

    20221108

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
   return (
     <>
-    <div className="h-12 bg-amber-300">
-      <h1 className="text-2xl">Enowiz</h1>
-    </div>
-    <div className="flex flex-row">
-      <div className="h-screen basis-1/4 bg-teal-300">01</div>
-      <div className="basis-3/4">09</div>
-    </div>
+    <Router>
+
+      <div className="h-24 bg-amber-300">
+        <h1 className="text-2xl">Enowiz</h1>
+        <div >
+          <ul className="flex space-x-5">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+              <Link to="/portfolio">Portfolio</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to="/about">About</Link>
+          </ul>
+
+        </div>
+      </div>
+    </Router>
+      <div className="flex flex-row">
+        <div className="h-screen basis-1/4 bg-teal-300">01</div>
+        <div className="basis-3/4">09</div>
+      </div>
     </>
   );
 }
