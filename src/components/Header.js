d6d797ce76bb5188d19a95a14319bf642cd74d8a
Enoch Park <enoch.park@enowiz.com>
2023-01-10 21:38:26 -0800

    20230110

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header(){
   return (
     <div className="mt-20">
       <h1 className="text-3xl font-bold text-center">ENOWIZ</h1>
-      <h2 className="p-4 text-xl text-center">where innovation and wisdom meets to create new and better inventions!</h2>
+      <h2 className="p-4 text-xl text-center">where innovation and wisdom meets to create new and better solutions!</h2>
     </div>
   )
 }
